5ede43fe15d9eae4248008ea72efb6b0eff9cf5c
kyechan99 <kyechan99@naver.com>
2017-02-14 15:17:27 +0900

    :bookmark: 0.3.0

---
+++ Assets/Script/Manager/Intro/Cartoon.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cartoon : MonoBehaviour
+{
+    [SerializeField]
+    UnityEngine.UI.Image[] cartoon;         // 카툰 5개 공간
+    [SerializeField]
+    GameObject cartoonCanvas;
+
+    [SerializeField]
+    GameObject titleObj;
+
+    int cutIdx = 0;     // 카툰 컷 씬 인덱스
+
+    void Start()
+    {
+
+        SMng.Instance.hideWeapon.SetActive(true);
+        SMng.Direction = 3;
+    }
+
+    /**
+     * @brief 다음 컷 씬으로 이동
+     */
+    public void nextCut()
+    {
+        cutIdx++;
+        //cartoon[0].sprite = Resources.Load<Sprite>("Cartoon/" + cutIdx);
+        if (cutIdx.Equals(2))
+        {
+            titleObj.SetActive(true);
+        }
+        else if (cutIdx.Equals(3))
+        {
+            gameStart();
+        }
+    }
+
+    [SerializeField]
+    Transform targetHero;
+    [SerializeField]
+    CameraFollow cam;
+
+    public void gameStart()
+    {
+
+        SMng.Instance.hideWeapon.SetActive(true);
+        SMng.Direction = 3;
+        cam.target = targetHero;
+
+        if (GM.LevelManager.myLevel.Equals(0))
+        {
+            GM.AudioManager.instance.rainBG();
+            SMng.Instance._level.StartCoroutine("direct_0");
+            //StartCoroutine(SMng.Instance._level.direct_0());
+        }
+        cartoonCanvas.SetActive(false);
+    }
+
+    // [0]-> 0, 1, 2
+    // [1]-> 3, 4
+    // [2]-> 5, 6
+    // [3]-> 7, 8
+    // [4]-> 9, 10
+
+
+
+
+}
---
+++ Assets/Script/Manager/System/LevelManager.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace GM
+{
+    public class LevelManager : MonoBehaviour
+    {
+        //public GameObject loadingCanvas;
+        public static int myLevel = 0;
+
+        [SerializeField]
+        GameObject introCanvas;
+        [SerializeField]
+        GameObject loadingObj;
+
+        // LevelManager.cs
+        [SerializeField]
+        Animator thunderScreenAnimator;
+        [SerializeField]
+        Animator thunderAnimator;
+        
+        [SerializeField]
+        GameObject Sit;
+        [SerializeField]
+        Sprite Sit_;
+
+
+        void Start()
+        {
+            //DontDestroyOnLoad(this);
+
+            if (myLevel.Equals(0))
+            {
+                introCanvas.SetActive(true);
+            }
+        }
+
+        public IEnumerator loading(bool isClear)
+        {
+
+            SMng.Instance.hideWeapon.SetActive(true);
+            SMng.Direction = 3;
+
+            SMng.interection = true;
+
+            loadingObj.SetActive(true);
+
+            yield return new WaitForSeconds(1);
+            if (isClear)
+            {
+                myLevel++;
+            }
+            else
+            {
+
+            }
+
+            SceneManager.LoadScene("Game");
+            yield return new WaitForSeconds(4);
+
+            SMng.Instance.hideWeapon.SetActive(false);
+            SMng.Direction = 0;
+
+            SMng.interection = false;
+        }
+
+
+       
+
+        public IEnumerator direct_0()
+        {
+            // 불 꺼짐
+            yield return new WaitForSeconds(8);
+            thunderAnimator.SetTrigger("Light0");
+            yield return new WaitForSeconds(1);
+            thunderScreenAnimator.SetBool("Bright", false);
+            thunderScreenAnimator.SetTrigger("Thunder");
+
+
+            // 불 켜짐
+            yield return new WaitForSeconds(15);
+            thunderAnimator.SetTrigger("Light1");
+            yield return new WaitForSeconds(1);
+            thunderScreenAnimator.SetBool("Bright", true);
+            thunderScreenAnimator.SetTrigger("Thunder");
+            Sit.GetComponent<SpriteRenderer>().sprite = Sit_;
+
+            SMng.Instance.hideWeapon.SetActive(false);
+            SMng.Instance.Hero.SetActive(true);
+            SMng.Direction = 0;
+
+            // 불 꺼짐
+            yield return new WaitForSeconds(9);
+            thunderAnimator.SetTrigger("Light0");
+            yield return new WaitForSeconds(1);
+            thunderScreenAnimator.SetBool("Bright", false);
+            thunderScreenAnimator.SetTrigger("Thunder");
+
+            // 불 켜짐
+            yield return new WaitForSeconds(16);
+            thunderAnimator.SetTrigger("Light1");
+            yield return new WaitForSeconds(1);
+            thunderScreenAnimator.SetBool("Bright", true);
+            thunderScreenAnimator.SetTrigger("Thunder");
+        }
+    }
+}
